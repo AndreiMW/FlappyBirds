b27440ff4480b58d3f6e6173c56e71f269eb5d1d
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-05 12:22:41 +0300

    Added basic implementation for GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+/**
+ * Created Date: 4/5/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Avangarde Software. All rights reserved. 
+ */
+
+
+using System;
+using Scripts;
+using UnityEngine;
+
+namespace Managers {
+	public class GameManager : MonoBehaviour {
+		private static GameManager s_instance;
+		public static GameManager Instance => s_instance ??= FindObjectOfType<GameManager>();
+
+		[SerializeField]
+		private ObstacleManager _obstacleManager;
+
+		[SerializeField]
+		private BirdController _birdController;
+
+		private void Update() {
+			if (Input.GetKeyDown(KeyCode.A)) {
+				this.StartGame();
+			}
+		}
+
+		public void StartGame() {
+			this._obstacleManager.StartSpawningObstacles();
+			this._birdController.EnableGravity();
+		}
+
+		public void EndGame() {
+			this._obstacleManager.StopSpawningObstacles();
+			this._birdController.DisableGravity();
+		}
+		
+	}
+}
