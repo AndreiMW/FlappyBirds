cf488c946a71bac4f014bd2c613875db5dd6aa5b
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-05 11:31:33 +0300

    Added logic to return obstacle to pool when it is out of bounds

--- Assets/Scripts/Managers/ObstacleManager.cs
+++ Assets/Scripts/Managers/ObstacleManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Obstacle;
 using UnityEngine;
 
 /**
@@ -8,16 +10,24 @@ using UnityEngine;
  * Copyright (c) 2023 Avangarde Software. All rights reserved. 
  */
 
-public class ObstacleManager : MonoBehaviour {
-	[SerializeField]
-	private ObstaclePool _obstaclePool;
+namespace Managers {
+	public class ObstacleManager : MonoBehaviour {
+		[SerializeField]
+		private ObstaclePool _obstaclePool;
 
 	
-	private void Awake() {
-		InvokeRepeating(nameof(this.GetObstacle),0f,2f);
-	}
+		private void Awake() {
+			InvokeRepeating(nameof(this.GetObstacle),0f,2f);
+		}
 
-	private void GetObstacle() {
-		this._obstaclePool.Get();
+		private void GetObstacle() {
+			ObstacleGroup obstacleGroup = this._obstaclePool.Get();
+			obstacleGroup.OnOutOfBounds += Listener;
+
+			void Listener(ObstacleGroup group) {
+				this._obstaclePool.Return(group);
+				obstacleGroup.OnOutOfBounds -= Listener;
+			}
+		}
 	}
 }
